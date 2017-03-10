939f82a2499b0afda843e651daf3623c411588b5
Anthony Leif <tonyleif@hotmail.com>
2017-03-09 23:44:56 -0600

    Initial add

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new RestaurantReview()
+            {
+                Name = "Tersiguel's",
+                Rating = 9
+            };
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Location = "Maryland, USA";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
